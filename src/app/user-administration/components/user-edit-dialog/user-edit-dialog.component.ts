337d5d60eb1fea15486de114c95ab6afdd544365
Sergiu Savu <Sergiu.Savu@msg.group>
2023-08-26 15:05:00 +0300

    Small changes

--- src/app/user-administration/components/user-edit-dialog/user-edit-dialog.component.ts
+++ src/app/user-administration/components/user-edit-dialog/user-edit-dialog.component.ts
@@ -127,13 +127,24 @@ export class UserEditDialogComponent implements OnInit {
               "USER.SUCCESS",
               "USER.INFO"
             ]).subscribe(translations => {
-              this.messageService.add({severity: 'success', summary: translations["USER.SUCCESS"], detail: translations["USER.MODIFIED"]});
-              if (this.registerForm.controls.active.dirty){
-                if(!this.registerForm.controls.active.value){
-                  this.messageService.add({severity: 'info', summary: translations["USER.INFO"], detail: translations["USER.DEACTIVATED"]});
-                }
-                else {
-                  this.messageService.add({severity: 'info', summary: translations["USER.INFO"], detail: translations["USER.ACTIVATED"]});
+              this.messageService.add({
+                severity: 'success',
+                summary: translations["USER.SUCCESS"],
+                detail: translations["USER.MODIFIED"]
+              });
+              if (this.registerForm.controls.active.dirty) {
+                if (!this.registerForm.controls.active.value) {
+                  this.messageService.add({
+                    severity: 'info',
+                    summary: translations["USER.INFO"],
+                    detail: translations["USER.DEACTIVATED"]
+                  });
+                } else {
+                  this.messageService.add({
+                    severity: 'info',
+                    summary: translations["USER.INFO"],
+                    detail: translations["USER.ACTIVATED"]
+                  });
                 }
               }
             });
@@ -144,7 +155,13 @@ export class UserEditDialogComponent implements OnInit {
           }
         }
       ))
-      .subscribe();
+      .subscribe(
+        {
+          next: (user => {
+            this.userFromDB = user;
+          })
+        }
+      );
   }
 
   hideDialog() {
